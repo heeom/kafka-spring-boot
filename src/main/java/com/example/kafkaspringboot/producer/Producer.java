5352557305c0ca8c497106b336c3de957a54df0a
heeom <swimxdev@gmail.com>
2025-02-01 17:59:04 +0900

    feat : Producer 구현

---
+++ src/main/java/com/example/kafkaspringboot/producer/Producer.java
@@ -0,0 +1,32 @@
+package com.example.kafkaspringboot.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class Producer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String topic, String value) {
+        String key = UUID.randomUUID().toString();
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, UUID.randomUUID().toString(), value);
+        future.whenComplete((result, e) -> {
+            if (e == null) {
+                logger.info("send message. topic : {}, key : {}, value : {}", topic, key, value);
+            }
+        });
+    }
+}
