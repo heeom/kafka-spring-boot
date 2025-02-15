af87be65017b5333781ce96fae3e0ad73ed97964
heeom <swimxdev@gmail.com>
2025-02-15 18:11:22 +0900

    feat : kafka producer로 메세지 전송

--- producers/src/main/java/com/example/producers/SimpleProducer.java
+++ producers/src/main/java/com/example/producers/SimpleProducer.java
@@ -1,18 +1,35 @@
 package com.example.producers;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.springframework.stereotype.Component;
 
-@Component
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class SimpleProducer {
 
-    private final KafkaProducer<String, String> kafkaProducer;
-
-    public SimpleProducer(final KafkaProducer<String, String> kafkaProducer) {
-        this.kafkaProducer = kafkaProducer;
-    }
+    private static final Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
 
     public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "test-topic";
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "hello-word");
+
+        // send message
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
     }
 
 }
