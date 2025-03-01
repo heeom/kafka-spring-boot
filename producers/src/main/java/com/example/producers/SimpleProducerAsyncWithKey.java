ad60afd15fa53582f6b7c2db70e3de0340ef5e68
heeom <swimxdev@gmail.com>
2025-03-01 13:53:21 +0900

    feat : add acks config

--- producers/src/main/java/com/example/producers/SimpleProducerAsyncWithKey.java
+++ producers/src/main/java/com/example/producers/SimpleProducerAsyncWithKey.java
@@ -20,6 +20,7 @@ public class SimpleProducerAsyncWithKey {
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "0");
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         String topic = "multipart-topic";
